cff05c1c4dd8cbd335a049535e75064c760b9d7a
ankin <akinash@gmail.com>
2013-02-13 16:21:44 +0100

    jQuery version update. Escape HTML characters in user generated code was added

--- learny-war/src/main/webapp/js/app/comment/commentsCol.js
+++ learny-war/src/main/webapp/js/app/comment/commentsCol.js
@@ -1,10 +1,11 @@
-define([ 'backbone' , 'common/baseCol'], function(Backbone, BaseCollection) {
+define([ 'backbone', 'common/baseCol'], function(Backbone, BaseCollection) {
 
     var commentCollection = BaseCollection.extend({
         initialize : function() {
             BaseCollection.prototype.initialize.apply(this, arguments);
         },
         url : 'services/comment/record/getall'
+
     });
     return commentCollection;
 });
