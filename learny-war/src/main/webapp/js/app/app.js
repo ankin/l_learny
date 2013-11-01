9ae49c964aeb6a4b047d0205cdf9df3f93e17472
ankin <akinash@gmail.com>
2013-11-01 15:24:56 +0100

    libs update

--- learny-war/src/main/webapp/js/app/app.js
+++ learny-war/src/main/webapp/js/app/app.js
@@ -1,7 +1,7 @@
 requirejs.config({
 
     paths : {
-        jquery : '../lib/jquery-1.10.1',
+        jquery : '../lib/jquery-2.0.3',
         lodash : '../lib/lodash-1.2.1',
         backbone : '../lib/backbone-1.0.0',
         jqueryDateFormat : '../lib/jquery.dateFormat-1.0',
